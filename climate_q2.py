acd80e8f9ea259d330a345748fed13e7fabadf53
Huy Liêu Gia <giahuy.lieu@griffith.edu.au>
2023-09-15 10:21:52 +1000

    Finish question 2

--- climate_q2.py
+++ climate_q2.py
@@ -5,16 +5,25 @@ years = []
 co2 = []
 temp = []
 
+pd.set_option('display.max_columns', 20)
+pd.set_option('display.max_colwidth', 20)
+pd.set_option('display.expand_frame_repr', False)
+data = pd.read_csv('climate.csv')
+
+years = data['Year'].tolist()
+co2 = data['CO2'].tolist()
+temp = data['Temperature'].tolist()
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
 
