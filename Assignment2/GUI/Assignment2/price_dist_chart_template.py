eaa04684614baa4747f1d7d7af405568e626d36b
Georgia <georgia.platt@griffithuni.edu.au>
2023-09-28 22:27:49 +1000

    finished Price Distribution Chart window (dataset added, graph showing & filters work)

--- Assignment2/GUI/Assignment2/price_dist_chart_template.py
+++ Assignment2/GUI/Assignment2/price_dist_chart_template.py
@@ -17,7 +17,7 @@ import wx.xrc
 class MyFrame3 ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 586,386 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 1000,600 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
@@ -41,72 +41,11 @@ class MyFrame3 ( wx.Frame ):
 
 		bSizer24.Add( self.PDC_suburb_label, 0, wx.ALL, 5 )
 
-		self.PDC_suburb_text = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.PDC_suburb_text.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		bSizer24.Add( self.PDC_suburb_text, 0, wx.ALL, 5 )
-
-		self.PDC_label = wx.StaticText( self, wx.ID_ANY, u"Rating:", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.PDC_label.Wrap( -1 )
-
-		self.PDC_label.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		bSizer24.Add( self.PDC_label, 0, wx.ALL, 5 )
-
-		gSizer4 = wx.GridSizer( 2, 5, 0, 0 )
-
-		self.m_checkBox21 = wx.CheckBox( self, wx.ID_ANY, u"1", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox21.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox21, 0, wx.ALL, 5 )
-
-		self.m_checkBox22 = wx.CheckBox( self, wx.ID_ANY, u"2", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox22.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox22, 0, wx.ALL, 5 )
-
-		self.m_checkBox23 = wx.CheckBox( self, wx.ID_ANY, u"3", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox23.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox23, 0, wx.ALL, 5 )
-
-		self.m_checkBox24 = wx.CheckBox( self, wx.ID_ANY, u"4", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox24.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox24, 0, wx.ALL, 5 )
-
-		self.m_checkBox25 = wx.CheckBox( self, wx.ID_ANY, u"5", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox25.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox25, 0, wx.ALL, 5 )
-
-		self.m_checkBox26 = wx.CheckBox( self, wx.ID_ANY, u"6", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox26.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox26, 0, wx.ALL, 5 )
-
-		self.m_checkBox27 = wx.CheckBox( self, wx.ID_ANY, u"7", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox27.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox27, 0, wx.ALL, 5 )
-
-		self.m_checkBox28 = wx.CheckBox( self, wx.ID_ANY, u"8", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox28.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox28, 0, wx.ALL, 5 )
-
-		self.m_checkBox29 = wx.CheckBox( self, wx.ID_ANY, u"9", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox29.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox29, 0, wx.ALL, 5 )
-
-		self.m_checkBox30 = wx.CheckBox( self, wx.ID_ANY, u"10", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_checkBox30.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
-
-		gSizer4.Add( self.m_checkBox30, 0, wx.ALL, 5 )
-
-
-		bSizer24.Add( gSizer4, 1, wx.EXPAND, 5 )
+		PDC_suburb_dropdownChoices = []
+		self.PDC_suburb_dropdown = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, PDC_suburb_dropdownChoices, 0 )
+		self.PDC_suburb_dropdown.SetSelection( 0 )
+		bSizer24.Add( self.PDC_suburb_dropdown, 0, wx.ALL, 5 )
+		self.PDC_suburb_dropdown.SetFont( wx.Font(12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial"))
 
 		self.PDC_property_type_label = wx.StaticText( self, wx.ID_ANY, u"Property Type:", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.PDC_property_type_label.Wrap( -1 )
@@ -115,10 +54,12 @@ class MyFrame3 ( wx.Frame ):
 
 		bSizer24.Add( self.PDC_property_type_label, 0, wx.ALL, 5 )
 
-		self.PDC_property_type_text = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.PDC_property_type_text.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
+		PDC_property_dropdownChoices = []
+		self.PDC_property_dropdown = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, PDC_property_dropdownChoices, 0 )
+		self.PDC_property_dropdown.SetSelection( 0 )
+		self.PDC_property_dropdown.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
 
-		bSizer24.Add( self.PDC_property_type_text, 0, wx.ALL, 5 )
+		bSizer24.Add( self.PDC_property_dropdown, 0, wx.ALL, 5 )
 
 		self.PDC_room_type_label = wx.StaticText( self, wx.ID_ANY, u"Room Type:", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.PDC_room_type_label.Wrap( -1 )
@@ -127,10 +68,12 @@ class MyFrame3 ( wx.Frame ):
 
 		bSizer24.Add( self.PDC_room_type_label, 0, wx.ALL, 5 )
 
-		self.PDC_room_text = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.PDC_room_text.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
+		PDC_room_dropdownChoices = []
+		self.PDC_room_dropdown = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, PDC_room_dropdownChoices, 0 )
+		self.PDC_room_dropdown.SetSelection( 0 )
+		self.PDC_room_dropdown.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
 
-		bSizer24.Add( self.PDC_room_text, 0, wx.ALL, 5 )
+		bSizer24.Add( self.PDC_room_dropdown, 0, wx.ALL, 5 )
 
 		self.PDC_search_button = wx.Button( self, wx.ID_ANY, u"Search", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.PDC_search_button.SetFont( wx.Font( 12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Arial" ) )
@@ -138,7 +81,7 @@ class MyFrame3 ( wx.Frame ):
 		bSizer24.Add( self.PDC_search_button, 0, wx.ALL, 5 )
 
 
-		bSizer23.Add( bSizer24, 1, wx.EXPAND, 5 )
+		bSizer23.Add( bSizer24, 0, wx.EXPAND, 5 )
 
 		bSizer25 = wx.BoxSizer( wx.VERTICAL )
 
@@ -158,32 +101,14 @@ class MyFrame3 ( wx.Frame ):
 		self.Centre( wx.BOTH )
 
 		# Connect Events
-		self.m_checkBox21.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
-		self.m_checkBox22.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
-		self.m_checkBox23.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
-		self.m_checkBox24.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
-		self.m_checkBox25.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
-		self.m_checkBox26.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
-		self.m_checkBox27.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
-		self.m_checkBox28.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
-		self.m_checkBox29.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
-		self.m_checkBox30.Bind( wx.EVT_CHECKBOX, self.rating_checkbox )
+		self.PDC_search_button.Bind( wx.EVT_BUTTON, self.PDC_search )
 
 	def __del__( self ):
 		pass
 
 
 	# Virtual event handlers, override them in your derived class
-	def rating_checkbox( self, event ):
+	def PDC_search( self, event ):
 		event.Skip()
-
-
-
-
-
-
-
-
-
 
 
--- Assignment2/GUI/Assignment2/price_dist_chart_template_run.py
+++ Assignment2/GUI/Assignment2/price_dist_chart_template_run.py
@@ -1,18 +1,83 @@
 import wx
-import wx.xrc
-import wx.adv
-import wx.grid
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
+from matplotlib.figure import Figure
 import pandas as pd
 
-# Replace 'your_module_name' with your actual module name
+data = pd.read_csv('listings_dec18.csv')
+
+data['price'] = data['price'].str.replace('$', '').str.replace(',', '').astype(float)
+price_data = data['price'].unique()
+price_data.sort()
+
+suburb_data = data['city']
+suburb_data_dropdown = data['city'].astype(str).unique()
+suburb_data_dropdown = [x.strip() for x in suburb_data_dropdown]
+suburb_data_dropdown.sort()
+
+property_data = data['property_type']
+property_data_dropdown = data['property_type'].unique()
+
+room_data = data['room_type']
+room_data_dropdown = data['room_type'].unique()
+
 from price_dist_chart_template import MyFrame3 as MyFrame1
 
-# Create a wx.App instance
-app = wx.App()
+class CalcFrame(MyFrame1):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-# Create an instance of your main frame
-frame = MyFrame1(None)
-frame.Show()
+        self.PDC_property_dropdown.SetItems(property_data_dropdown)
+        self.PDC_room_dropdown.SetItems(room_data_dropdown)
+        self.PDC_suburb_dropdown.SetItems(suburb_data_dropdown)
 
-# Start the main event loop
-app.MainLoop()
+        self.figure_score = Figure()
+        self.axes = self.figure_score.add_subplot(111)
+        self.canvas = FigureCanvasWxAgg(self.PDC_graph, -1, self.figure_score)
+        self.canvas.Hide()  # Initially hide the canvas
+
+        self.Layout()
+        self.Show(True)
+
+        self.Bind(wx.EVT_CHOICE, self.on_property_type_choice, self.PDC_property_dropdown)
+        self.Bind(wx.EVT_CHOICE, self.on_room_type_choice, self.PDC_room_dropdown)
+        self.Bind(wx.EVT_CHOICE, self.on_suburb_choice, self.PDC_suburb_dropdown)
+
+        # track if a filter has changed
+        self.filter_changed = False
+
+    def PDC_search(self, event):
+        if self.filter_changed:
+            self.plot_data_line()
+            self.filter_changed = False
+
+    def on_property_type_choice(self, event):
+        self.filter_changed = True
+
+    def on_room_type_choice(self, event):
+        self.filter_changed = True
+
+    def on_suburb_choice(self, event):
+        self.filter_changed = True
+
+    def plot_data_line(self):
+        selected_property = self.PDC_property_dropdown.GetStringSelection()
+        selected_room = self.PDC_room_dropdown.GetStringSelection()
+        selected_suburb = self.PDC_suburb_dropdown.GetStringSelection()
+
+        filtered_data = data[(data['property_type'] == selected_property) & (data['room_type'] == selected_room) & (data['city'] == selected_suburb)]
+
+        self.axes.clear()  # Clear previous plot
+        x_series = pd.Series(filtered_data['price'])
+        self.axes.bar(range(len(x_series)), x_series)
+        self.axes.set_title(f'Price Distribution for {selected_property}s in {selected_suburb} ({selected_room})')
+        self.axes.set_ylabel('Price')
+        self.axes.set_xlabel('No. of Listings')
+
+        # Refresh the canvas
+        self.canvas.draw_idle()
+        self.canvas.Show()
+
+if __name__ == "__main__":
+    app = wx.App(False)
+    frame = CalcFrame()
+    app.MainLoop()
